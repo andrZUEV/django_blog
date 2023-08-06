c03aa2a8935a804d1157aa5b25171363b87013d9
andrZUEV <labazz76@gmail.com>
2023-08-06 14:07:17 +0300

    Change the HTML for the site

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+'''Свяжем view с именем post_list  с корневым URL-адресом(''), имена URL должны быть понятными и уникальными'''
+
+urlpatterns = [
+    path('', views.post_list, name = 'post_list')
+]
