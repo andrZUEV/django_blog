bf56dcbfb7ae5c03a7fc76d6911d4ed89412a840
andrZUEV <labazz76@gmail.com>
2023-08-06 15:58:20 +0300

    Modified templates to displays posts from database

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Post # . перед models указывает на то, что мы обращаемся к файлу из текущей деректории
+#деректории в которой лежит данный фал views.py
+
+#Импортируем временные зоны 
+from django.utils import timezone
+
 
 def post_list(request):
-    return render (request, 'blog/post_list.html' , {})
+    #Отсортируем и отфильтруем посты по дате публикации
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render (request, 'blog/post_list.html' , {'posts' : posts})
